342e5ec690ac58dc7aa18a4832c09bce4921bd2e
Vinicius Becker P <100175706+Beckervini@users.noreply.github.com>
2025-06-11 14:38:52 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-  ],
+  base: "/Curriculnterativo2-main/", // Caminho do repositório no GitHub
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
