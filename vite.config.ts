724ee0596b7613e62157c7e796d964c0afe92433
Vinicius Becker <beckerviniprediger@gmail.com>
2025-06-12 12:40:18 -0300

    correção

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig({
-  
-  base: "/Teste2/", 
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ command }) => { 
+  return { 
+    
+    base: command === 'build' ? '/Teste2/' : '/', 
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  }; 
 });
